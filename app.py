79eb4e5f9bb4f4e92e994ce8feacb5429ff3a1e7
Youngsoo <newpullin@gmail.com>
2020-09-08 22:18:21 +0900

    finish mini twitter

--- app.py
+++ app.py
@@ -85,3 +85,10 @@ def timeline(user_id):
         return 'no user', 400
     
     follow_list = app.users[user_id].get('follow', set())
+    follow_list.add(user_id)
+    timeline = [tweet for tweet in app.tweets if tweet['user_id'] in follow_list]
+
+    return jsonify({
+        'user_id' : user_id,
+        'timeline': timeline
+    })
